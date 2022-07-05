81db5b790294adf3ddc86f6ec2c5545da8c4bf4a
Aleksandra289 <vujovic.it39.2019@uns.ac.rs>
2022-07-05 16:08:16 +0200

    front

--- IT-39-2019-RVA-FrontEnd/src/app/constants.ts
+++ IT-39-2019-RVA-FrontEnd/src/app/constants.ts
@@ -1,8 +1,8 @@
 
-export const RACUN_URI = 'http://localhost:8083/racun';
-export const PROIZVODJAC_URI = 'http://localhost:8083/proizvodjac';
-export const PROIZVOD_URI = 'http://localhost:8083/proizvod';
-export const STAVKA_RACUNA_URI = 'http://localhost:8083/stavkaRacuna';
-export const STAVKA_FOR_RACUN_URI = 'http://localhost:8083/stavkaRacuna/racun';
+export const RACUN_URI = 'https://rvafinalapp.herokuapp.com/racun';
+export const PROIZVODJAC_URI = 'https://rvafinalapp.herokuapp.com/proizvodjac';
+export const PROIZVOD_URI = 'https://rvafinalapp.herokuapp.com/proizvod';
+export const STAVKA_RACUNA_URI = 'https://rvafinalapp.herokuapp.com/stavkaRacuna';
+export const STAVKA_FOR_RACUN_URI = 'https://rvafinalapp.herokuapp.com/stavkaRacuna/racun';
 
 
